c1d7aada3a4f4c45c0f6d48771a128827f9ce111
Nanana291 <156972279+Nanana291@users.noreply.github.com>
2026-06-12 23:04:43 -0700

    Add AbilityArena script URL to Games table

--- Loader.lua
+++ Loader.lua
@@ -2,6 +2,7 @@ local Games = {
     [9073513091] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/AnimeApocalypse.lua",
     [3647333358] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/Evade.lua",
     [9910245722] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/IronSoulDungeon.lua",
+    [10230942274] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/AbilityArena.lua",
 }
 
 local ScriptURL = Games[game.GameId]
