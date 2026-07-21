b3a5184134a9aff359f15fd31af16e0fb9d97e1e
Nanana291 <156972279+Nanana291@users.noreply.github.com>
2026-07-20 20:59:39 -0700

    Add GardenTD script URL to Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -18,6 +18,7 @@ local Games = {
     [10406668651] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/MinePlanet.lua",
     [10421154706] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/BeFinalBoss.lua",
     [9330616906] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/BeABait.lua",
+    [7703614594] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/GardenTD.lua",
 }
 local ScriptURL = Games[game.GameId]
 
