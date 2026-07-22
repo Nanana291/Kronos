db4ef5b204321825c6fcb528e176d7fcbbe33bba
Nanana291 <156972279+Nanana291@users.noreply.github.com>
2026-07-21 21:48:12 -0700

    Add new script URL for +1PickaxeSwing

--- Loader.lua
+++ Loader.lua
@@ -20,6 +20,7 @@ local Games = {
     [9330616906] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/BeABait.lua",
     [7703614594] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/GardenTD.lua",
     [10209534490] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/ChickenFarm.lua",
+    [10456832990] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/%2B1PickaxeSwing.lua",
 }
 local ScriptURL = Games[game.GameId]
 
