898aac65acf4efb43c33765b06548db04dc360c1
Nanana291 <156972279+Nanana291@users.noreply.github.com>
2026-07-27 19:22:34 -0700

    Add AnimeCardFarm script URL to Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -29,6 +29,7 @@ local Games = {
     [9815834699] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/MorphUp.lua",
     [10454554751] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/UnboxAsmr.lua",
     [10376742546] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/BecomeBillonaire.lua",
+    [10144587520] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/AnimeCardFarm.lua",
 }
 local ScriptURL = Games[game.GameId]
 
