f4be6a3244406a41b6ce48a510eda2f05a6748e0
Nanana291 <156972279+Nanana291@users.noreply.github.com>
2026-08-09 17:47:29 -0700

    Add AnimeFighting script URL to Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -47,6 +47,7 @@ local Games = {
     [10410945205] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/%2B1CutGrass.lua",
     [8296765616] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/DefendBunker.lua",
     [10338952197] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/ChickenFighter.lua",
+    [10321202755] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/AnimeFighting.lua",
 }
 local ScriptURL = Games[game.GameId]
 
