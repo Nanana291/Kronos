3aca835393132dcfedd4c4618a32fbfe815f7dbe
Nanana291 <156972279+Nanana291@users.noreply.github.com>
2026-08-12 19:03:37 -0700

    Add HomerNightmare.lua script URL to Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -57,6 +57,7 @@ local Games = {
     [7822444776] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/BuildPlane.lua",
     [10506207587] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/MagicLoot.lua",
     [10539411000] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/CleanLeaves.lua",
+    [10335443723] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/HomerNightmare.lua",
 }
 local ScriptURL = Games[game.GameId]
 
