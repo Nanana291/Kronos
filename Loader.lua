ddbda5ebcad806329f7618d603feb59efbcc1b45
Nanana291 <156972279+Nanana291@users.noreply.github.com>
2026-08-17 22:18:46 -0700

    Add new script URL for DontBack.lua

--- Loader.lua
+++ Loader.lua
@@ -74,6 +74,7 @@ local Games = {
     [10423774136] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/ProtectBorder.lua",
     [8946565814] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/AnimeOrigins.lua",
     [10219827882] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/Automate.lua",
+    [10493730706] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/DontBack.lua",
 }
 local ScriptURL = Games[game.GameId]
 
