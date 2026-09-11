7e47fcb536dbd417900865c1ded8076b55f12b77
Nanana291 <156972279+Nanana291@users.noreply.github.com>
2026-09-10 19:19:36 -0700

    Add MilitaryTycoon script URL to Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -130,6 +130,7 @@ local Games = {
     [6215986499] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/GrandBlue.lua",
     [10759337137] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/LastStop.lua",
     [10700505394] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/LongToy.lua",
+    [10698216738] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/MilitaryTycoon.lua",
 }
 
 local HttpService = game:GetService("HttpService")
