00f77edf762cf1a9db38d1706c92338f48058dc9
Nanana291 <156972279+Nanana291@users.noreply.github.com>
2026-09-11 00:04:24 -0700

    Add TntMining script URL to Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -134,6 +134,7 @@ local Games = {
     [10708913337] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/AnimeDice.lua",
     [10552240401] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/DefeatAnime.lua",
     [10757690102] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/HowFish.lua",
+    [10418224975] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/TntMining.lua",
 }
 
 local HttpService = game:GetService("HttpService")
