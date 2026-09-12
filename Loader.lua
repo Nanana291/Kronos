cc98f0175e4be9dbc1ac97e9c34200b4b936854e
Nanana291 <156972279+Nanana291@users.noreply.github.com>
2026-09-11 18:24:08 -0700

    Add CommandArmy script URL to Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -136,6 +136,7 @@ local Games = {
     [10757690102] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/HowFish.lua",
     [10418224975] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/TntMining.lua",
     [10758034175] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/ReallyFish.lua",
+    [10258991999] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/CommandArmy.lua",
 }
 
 local HttpService = game:GetService("HttpService")
