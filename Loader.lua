654ecb252e368b6d8cddd160523a59a00acd6ac2
Nanana291 <156972279+Nanana291@users.noreply.github.com>
2026-09-20 12:38:24 -0700

    Add new script URL for Strength.lua

--- Loader.lua
+++ Loader.lua
@@ -143,6 +143,7 @@ local Games = {
     [9957338123] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/KartHell.lua",
     [10763764817] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/SwingEgg.lua",
     [10578398333] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/SurviveDesert.lua",
+    [10310999762] = "https://raw.githubusercontent.com/Nanana291/Kronos/refs/heads/main/Scripts/+1Strength.lua",
 }
 
 local HttpService = game:GetService("HttpService")
